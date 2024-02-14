2b5ebc918da9d1e4319f0f9aa9b34b252992390c
St3ph4n1e <boisne.stephanie@edu.esiee-it.fr>
2024-02-14 20:46:24 +0100

    template home

---
+++ app/src/main/java/com/example/spyproject/Game/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.example.spyproject.Game
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.spyproject.MainActivity
+import com.example.spyproject.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Écouteur de clic pour le bouton
+        binding.startButton.setOnClickListener {
+            // Lorsque le bouton est cliqué, lancer l'activité MainActivity
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
